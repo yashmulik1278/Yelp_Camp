f8d8cc62aca43bad70c3a1fe893c5e1a8e76e149
yashmulik1278 <yashmulik1278@gmail.com>
2024-03-11 13:12:23 +0530

    take-3

--- app.js
+++ app.js
@@ -86,11 +86,13 @@ app.use(
     })
 );
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret
     }
 });
 
@@ -101,7 +103,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
